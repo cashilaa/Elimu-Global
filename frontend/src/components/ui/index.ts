48c6896b4f956f4eda12667960e92c6f8f3fa03e
castysmith <castysmith739@gmail.com>
2024-12-21 08:13:50 +0300

    push

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,10 @@
+export { Badge, type BadgeProps } from './Badge'
+export { Button, type ButtonProps } from './Button'
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from './Card' 
