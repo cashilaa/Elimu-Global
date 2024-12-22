b5b521aeea4de6b1b49120396b7fb0e247091351
Michael Moses <167522583+micymike@users.noreply.github.com>
2024-12-22 17:10:46 +0300

    Delete interactive-learning-platform/Elimu Global directory

--- interactive-learning-platform/Elimu Global/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'; // Ensure this path is correct based on your project structure
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
